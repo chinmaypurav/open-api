ff5890c4253c355f9d1d45491e2880a96bb9c9f5
Chinmay Purav <chinpurav@gmail.com>
2023-07-18 01:51:14 +0530

    Operation added

---
+++ src/Operation.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Chinmay\OpenApi;
+
+class Operation
+{
+    /**
+     * @var array[string]
+     */
+    public array $tags;
+
+    /**
+     * @var string
+     */
+    public string $string;
+
+    /**
+     * @var string
+     */
+    public string $description;
+
+    public ExternalDocumentation $externalDocs;
+
+    /**
+     * @var string
+     */
+    public string $operationId;
+
+    public bool $deprecated = false;
+
+    public SecurityRequirement $security;
+
+    public array $servers;
+
+    /**
+     * @param array $tags
+     * @return Operation
+     */
+    public function setTags(array $tags): Operation
+    {
+        $this->tags = $tags;
+        return $this;
+    }
+
+    /**
+     * @param string $string
+     * @return Operation
+     */
+    public function setString(string $string): Operation
+    {
+        $this->string = $string;
+        return $this;
+    }
+
+    /**
+     * @param string $description
+     * @return Operation
+     */
+    public function setDescription(string $description): Operation
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @param ExternalDocumentation $externalDocs
+     * @return Operation
+     */
+    public function setExternalDocs(ExternalDocumentation $externalDocs): Operation
+    {
+        $this->externalDocs = $externalDocs;
+        return $this;
+    }
+
+    /**
+     * @param string $operationId
+     * @return Operation
+     */
+    public function setOperationId(string $operationId): Operation
+    {
+        $this->operationId = $operationId;
+        return $this;
+    }
+
+    /**
+     * @param bool $deprecated
+     * @return Operation
+     */
+    public function setDeprecated(bool $deprecated): Operation
+    {
+        $this->deprecated = $deprecated;
+        return $this;
+    }
+
+    /**
+     * @param SecurityRequirement $security
+     * @return Operation
+     */
+    public function setSecurity(SecurityRequirement $security): Operation
+    {
+        $this->security = $security;
+        return $this;
+    }
+
+    /**
+     * @param array $servers [Server] $servers
+     * @return Operation
+     */
+    public function setServers(array $servers): Operation
+    {
+        $this->servers = $servers;
+        return $this;
+    }
+}
